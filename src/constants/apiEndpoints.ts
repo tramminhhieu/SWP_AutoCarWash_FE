7ea4dd8a29b1065132df60ef43d5868f50bdf957
tramminhhieu <tramminhhieu>
2026-07-14 21:40:32 +0700

    refactor(system_setting): change apiEndpoint name

--- src/constants/apiEndpoints.ts
+++ src/constants/apiEndpoints.ts
@@ -117,7 +117,7 @@ export const API = {
     SOFT_DELETE_CAMPAIGN: "/api/admin/promotions", // + /{promotionId}/soft-delete
     SOFT_DELETE_VOUCHER: "/api/admin/vouchers", // + /{voucherId}/soft-delete
   },
-  SETTINGS: {
+  SYSTEM_SETTINGS: {
     LIST: "/api/admin/system-settings",
     CREATE: "/api/admin/system-settings",
     UPDATE: (id: number) => `/api/admin/system-settings/${id}`,
